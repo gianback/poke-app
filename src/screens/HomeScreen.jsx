2f156831206c44be6be86cb473cff38748de9dcd
Gian Franco <gian_franco_08@hotmail.com>
2022-06-27 21:21:51 -0500

    finished

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -19,7 +19,7 @@ const HomeScreen = () => {
             </div>
             <div className="flex justify-center items-start md:items-center">
               <Link to="/all">
-                <button className="uppercase rounded-lg py-3 px-4 bg-yellow-300 text-red-500 font-bold  text-2xl lg:text-3xl">
+                <button className="uppercase flex gap-5 rounded-lg py-3 px-4 bg-yellow-300 text-red-500 font-bold  text-2xl lg:text-3xl">
                   Start
                   <img
                     className="w-14 mx-auto mt-2"
